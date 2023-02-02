4dff049b1f7d590b198e6bd704703f23ac5e86f3
Eliran Eliassy <eliran.eliassy@gmail.com>
2023-02-02 11:27:56 +0200

    Initial commit

---
+++ apps/books-store/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bmc-b-course-project-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'books-store';
+}
