d8ec995d660aad110d8acbea207a99e84ee83061
Eliran Eliassy <eliran.eliassy@gmail.com>
2023-02-16 15:52:55 +0200

    value accessor

--- libs/ui-components/generic-input/generic-input.component.ts
+++ libs/ui-components/generic-input/generic-input.component.ts
@@ -1,14 +1,28 @@
-import { ChangeDetectionStrategy, Component, Input, NgModule } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Input, NgModule, ViewChild, AfterViewInit, Self, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, ControlValueAccessor, FormsModule, NgControl, NG_VALIDATORS, NG_VALUE_ACCESSOR, ReactiveFormsModule, ValidationErrors, Validator, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'b-input',
   templateUrl: './generic-input.component.html',
   styleUrls: ['./generic-input.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    // {
+    //   provide: NG_VALUE_ACCESSOR,
+    //   multi: true,
+    //   useExisting: GenericInputComponent
+    // },
+    // {
+    //   provide: NG_VALIDATORS,
+    //   multi: true,
+    //   useExisting: GenericInputComponent
+    // }
+  ]
 })
-export class GenericInputComponent {
+export class GenericInputComponent implements ControlValueAccessor, Validator, AfterViewInit, OnInit {
+
+
   @Input() type = 'text';
   @Input() required = false;
   @Input() pattern: string | null = null;
@@ -16,6 +30,69 @@ export class GenericInputComponent {
   @Input() label?: string;
   @Input() placeholder?: string;
   @Input() errorMsg?: string;
+
+  @ViewChild('input') input!: ElementRef;
+
+  disabled = false;
+
+  constructor(@Self() public controlDir: NgControl) {
+    this.controlDir.valueAccessor = this;
+  }
+  ngOnInit(): void {
+    const control = this.controlDir.control;
+    const validators: ValidatorFn[] = control?.validator ? [control.validator] : [];
+
+    if(this.required) {
+      validators.push(Validators.required);
+    }
+
+    if(this.pattern) {
+      validators.push(Validators.pattern(this.pattern))
+    }
+
+    control?.setValidators(validators);
+    control?.updateValueAndValidity();
+  }
+
+  writeValue(obj: any): void {
+    if (this.input) {
+      this.input.nativeElement = obj;
+    }
+
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    const validators: ValidatorFn[] = [];
+    if (this.required) {
+      validators.push(Validators.required);
+    }
+    if (this.pattern) {
+      validators.push(Validators.pattern(this.pattern))
+    }
+
+    return validators;
+  }
+
+
+  onTouched() {
+
+  }
+
+  onChange(value: string) {
+
+  }
+  ngAfterViewInit(): void {
+    // console.log(this.input)
+  }
 
 }
 
